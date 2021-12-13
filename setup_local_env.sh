2194e972ed85aa9249be37c2e1b1307c6cc54b6b
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-12-13 23:30:17 +0100

    Init

---
+++ setup_local_env.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/cryptobench/golemgrid-backend
+git clone https://github.com/cryptobench/golemgrid-container-manager
+git clone https://github.com/cryptobench/golemgrid-frontend
+mkdir uploads
